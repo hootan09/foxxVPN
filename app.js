624aa3a73ac52a476f2fdc8784670634ea7df9b3
niki <hootan09@gmail.com>
2021-10-07 16:44:25 +0330

    change to json

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/', function(req, res) {
   try {
     const requestData = logRequest(req);
     if(requestData.originalUrl == '/'){
-      return res.status(200).send({
+      return res.status(200).json({
         uptime: process.uptime(),
         title: process.title,
         version: process.version,
